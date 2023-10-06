3711856e0960ca209bc2e90aa87251e7ec8d51df
zsombinagy <zsombinagy.14@gmail.com>
2023-10-06 19:07:45 +0200

    dome manipulation

--- script.js
+++ script.js
@@ -1,68 +1,27 @@
-let x = "asd";
-let y = 12;
-let z = false;
+console.log("hello world");
 
-
-let arr = ["egy", "kettő", "három",];
-let arr2 = [10, 2, 67];
-let arr3 = [true, false, true];
-let arr4 = ["string", 1, false];
-
-
-
-let obj1 = {
-    key1: "value",
-    key2: 101,
-    key3: false,
-    key2: [
-        "cat",
-        "dog",
-        "chicken"
-    ],
-    users: [
-        {
-            name: "Gipsz Jakab",
-            age: 28,
-            isActive: true,
-            pets: {
-                name: "Mirci",
-                type: "cat"
-            }
-
-        }, 
-        {
-            name: "John Doe",
-            age: 23,
-            isActive: false,
-            pets: {
-                name: "Gordon",
-                type: "cat"
-            }
-        
-        }
-    ]
+function domManipulation() {
+  const rootElement = document.querySelector(`#root`);
+  console.log(rootElement);
+  rootElement.innerHTML = "this is done by dom manipulation";
 }
 
 
 
-function addTwoNumbers(number1, number2) {
-    let sum = number1 + number2
-    console.log(`the sum of two number(${number1}, ${number2}) is: ${sum}`)
-    return sum
+function logClick() {
+    console.log("clicked")
+
+
 }
 
 
-let resultOfAddTwoNumbers = addTwoNumbers(10, 2)
-console.log(`the result of resultOfAddTwoNumbers is: ${resultOfAddTwoNumbers}`)
+function logTick() {
+    
+    
 
-function cbExample() {
-    console.log("i am a callback function")
 }
 
-function funcExample(name, callback) {
-    console.log(`hello ${name}`)
-    callback()
-}
+window.addEventListener("load", domManipulation);
+window.addEventListener("click", logTick)
 
-funcExample("Ricsi", cbExample)
- 
+window.setInterval(logTick, 1000)
