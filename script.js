2c2f5c09db57152d7e10afb85e2506fcd4e6f60b
zsombinagy <zsombinagy.14@gmail.com>
2023-10-09 20:09:14 +0200

    builtins

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-const arr = [1 ,2 ,3 ,4 ,5]
+const users = [
+{ 
+  name: "Zsombor Nagy",
+  age: 20 
+},
+{ 
+  name: "Illés Bendegúz",
+  age: 20 
+},
+{ 
+  name: "Lendvai Bálint",
+  age: 20 
+},
+{ 
+  name: "Balázs Ármin",
+  age: 20 
+},
+];
 
-/* function returnDouble(number) {
-    return number * 2
-}
+const userNames = users.map((user, index) => {
+  return {
+    name: user.name,
+    age: user.age,
+    id: index + 1
+  }
+})
 
-const returnDouble2 = number => number * 2
-
-for (const item of arr) {
-    console.log(returnDouble2(item))
-} */
-
-/* function logDouble(number) {
-    console.log( element * 2)
-}
-*/
-arr.forEach(logger = number => number * 2)
-
+console.log(userNames)
